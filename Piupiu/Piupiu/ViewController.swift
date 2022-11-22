1402ecf9ebc27e42e49620197eb8cf5b15f86a67
miguel_the_flipnote_frog <96708315+MiguelFogli@users.noreply.github.com>
2022-11-22 18:42:09 -0300

    add files

---
+++ Piupiu/Piupiu/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Piupiu
+//
+//  Created by Miguel Braga Fogli on 22/11/22.
+//
+
+import Cocoa
+import WebKit
+
+class ViewController: NSViewController {
+    @IBOutlet weak var webview: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        webview.load(URLRequest(url: URL(string: "https://kooapp.com")!))
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
